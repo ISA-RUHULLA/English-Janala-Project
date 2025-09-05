caff139a3265bc25bbe9dba4778570bcd059cb9e
unknown <isaruhullah035@gmail.com>
2025-09-05 06:30:18 +0600

    Error Handle

--- script.js
+++ script.js
@@ -5,24 +5,46 @@ const loadLesson = () => {
 
 };
 
+const removeActiveClass = () => {
+    const buttons = document.getElementsByClassName('lesson-btn');
+    for (const button of buttons) {
+        button.classList.remove('bg-blue-700', 'text-white');
+    }
+};
+
 const loadLessonWord = (id) => {
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
         .then(response => response.json())
-        .then(json => displayLessonWord(json.data))
+        .then(json => {
+            removeActiveClass();
+            const clickbtn = document.getElementById(`lesson-btn-${id}`);
+            clickbtn.classList.add('bg-blue-700', 'text-white');
+            displayLessonWord(json.data)
+        });
 };
 const displayLessonWord = (words) => {
     console.log(words)
     const lessonWordContainer = document.getElementById('load-lesson-word');
     lessonWordContainer.innerHTML = '';
 
+    if (words.length === 0) {
+        lessonWordContainer.innerHTML = `
+            <div class="text-center col-span-3">
+                <img src="./assets/alert-error.png" alt="Alert Error" class="mx-auto item-center mb-5">
+                <p class="font-bangla text-[13px] space-y-6">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                <h1 class="font-bold text-xl">নেক্সট Lesson এ যান</h1>
+            </div>`;
+        return;
+    }
+
     words.forEach(word => {
         console.log(word)
         const lessonWordDiv = document.createElement('div');
         lessonWordDiv.innerHTML = `<div class="lesson-card bg-white rounded shadow py-10 px-2 mb-5 mr-4 space-y-5 ">
-            <h1 class="font-semibold md:font-bold text-2xl">${word.word}</h1>
-            <p class="font-semibold">"${word.meaning}/${word.pronunciation}"</p>
-            <div class="font-semibold font-bangla">${word.meaning}</div>
+            <h1 class="font-semibold md:font-bold text-2xl">${word.word ? word.word : 'শব্দ পাওয়া যায়নি'}</h1>
+            <p class="font-semibold">"${word.meaning ? word.meaning : 'অর্থ পাওয়া যায়নি'}/${word.pronunciation ? word.pronunciation : 'pronunciation পাওয়া যায় নি।'}"</p>
+            <div class="font-semibold font-bangla">${word.meaning ? word.meaning : 'অর্থ পাওয়া যায়নি'}</div>
             <div class="flex  justify-between p-10 mt-5">
                 <button class="btn bg-sky-200 rounded hover:bg-sky-400"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-sky-200 rounded hover:bg-sky-400"><i class="fa-solid fa-volume-high"></i></button>
@@ -48,7 +70,7 @@ const displayLesson = lessons => {
         // create the div
         const levelDiv = document.createElement('div');
         // create a button
-        levelDiv.innerHTML = `<button id="lesson-${lesson.level_no}" onclick="loadLessonWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no} </button>`;
+        levelDiv.innerHTML = `<button id="lesson-btn-${lesson.level_no}" onclick="loadLessonWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"><i class="fa-solid fa-book-open"></i> Lesson-${lesson.level_no} </button>`;
         // append the button to the container
         levelContainer.appendChild(levelDiv);
     };
